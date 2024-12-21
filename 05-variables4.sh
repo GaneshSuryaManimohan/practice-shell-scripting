cbe6e14d3b70c58d45e9cfc71baee2f1d6e08e84
manimohan <manimohan.t22@gmail.com>
2024-12-21 18:34:21 +0530

    Variables 4 script with O/P

--- 05-variables4.sh
+++ 05-variables4.sh
@@ -5,3 +5,11 @@ read USERNAME #here USERNAME is a variable, and read command allows you to enter
 echo "Please enter you password:: "
 read PASSWORD
 echo "The username is: $USERNAME and the password is: $PASSWORD"
+
+### OUTPUT ###
+#sh 05-variables4.sh
+#Please enter your username::
+#surya
+#Please enter you password::
+#surya242
+#The username is: surya and the password is: surya242
