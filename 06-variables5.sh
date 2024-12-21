8fa8a55c75a9027800a86a3eea40abb6a472a53e
manimohan <manimohan.t22@gmail.com>
2024-12-21 18:44:46 +0530

    Variables 5 script with O/P

--- 06-variables5.sh
+++ 06-variables5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Please enter your username:: "
-read USERNAME #here USERNAME is a variable, and read command allows you to enter a username on terminal
+read -s USERNAME #here USERNAME is a variable, and read command allows you to enter a username on terminal
 echo "Please enter you password:: "
-read -s PASSWORD #here the password you enter will not be displayed on the terminal
+read -s PASSWORD  #here the password you enter will not be displayed on the terminal
 echo "The username is: $USERNAME and the password is: $PASSWORD" #here you will get the password you entered just for our verification purpose
