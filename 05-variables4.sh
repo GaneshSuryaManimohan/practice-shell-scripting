e8d93f9401dd649a0c5d80d3d087f0a6b2a64b19
manimohan <manimohan.t22@gmail.com>
2024-12-21 18:32:46 +0530

    Variables 4 script created

---
+++ 05-variables4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Please enter your username:: "
+read USERNAME #here USERNAME is a variable, and read command allows you to enter a username on terminal
+echo "Please enter you password:: "
+read PASSWORD
+echo "The username is: $USERNAME and the password is: $PASSWORD"
