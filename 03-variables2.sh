a51ca6492e64930aab8088b8e144de4796c3eedd
manimohan <manimohan.t22@gmail.com>
2024-12-21 18:20:03 +0530

    Variables 2 script created

---
+++ 03-variables2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=Ramesh #no space between key and value
+PERSON2=Surya
+
+echo "$PERSON1:Hello, $PERSON2, How are you?"
+echo "$PERSON2:I'm fine $PERSON1, How about you?"
+echo "$PERSON1:I'm great!"
