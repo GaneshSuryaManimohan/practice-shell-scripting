98fec77390af9dc68ae4ebb10ad8ccb512764153
manimohan <manimohan.t22@gmail.com>
2024-12-21 19:15:37 +0530

    variables5

--- 06-variables5.sh
+++ 06-variables5.sh
@@ -2,6 +2,6 @@
 
 echo "Please enter your username:: "
 read USERNAME #here USERNAME is a variable, and read command allows you to enter a username on terminal
-stty echo "Please enter you password:: "
-#read - PASSWORD  #here the password you enter will not be displayed on the terminal
+echo "Please enter you password:: "
+read -s PASSWORD  #here the password you enter will not be displayed on the terminal
 echo "The username is: $USERNAME and the password is: $PASSWORD" #here you will get the password you entered just for our verification purpose
