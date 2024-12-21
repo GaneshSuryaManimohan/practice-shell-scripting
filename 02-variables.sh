cd6befd2f29a390c794f8aa6ef864ee8f38b2d47
manimohan <manimohan.t22@gmail.com>
2024-12-21 18:16:42 +0530

    Variables script created

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Ramesh: Hello, Suresh. How are you?"
+echo "Suresh: I'm fine thank you! How about you?"
+echo "Ramesh: I'm great!"
+
+#Here, whenever we need to change the names, instead of changing the code we can pass the variables, it is shown in the next script 03-variables.sh
