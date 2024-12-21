2bc144d0b84da2c6063f9c17588f7756ffcba503
manimohan <manimohan.t22@gmail.com>
2024-12-21 17:57:05 +0530

    new

---
+++ 01-hello-world.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#the above #!/bin/bash is shebang, it is the location interpreter, commands written inside shell script is interpreted and executed by this shebang
+echo "Hello World, I'm learning DevOps"
