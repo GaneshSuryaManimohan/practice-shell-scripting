92d514625e66e0794dc378e4bec4eb5c4c6636a5
manimohan <manimohan.t22@gmail.com>
2024-12-21 19:40:33 +0530

    array with O/P

--- 07-array.sh
+++ 07-array.sh
@@ -5,3 +5,7 @@ movies=("Pushpa2" "Mufasa" "UI")
 echo "The first movie is ${movies[0]}"
 echo "The second movie is ${movies[1]}"
 echo "All movies are ${movies[@]}"
+
+# The first movie is Pushpa2
+# The second movie is Mufasa
+# All movies are Pushpa2 Mufasa UI
