e435a38e9a71d3eb3611314836d505dc14cb3b15
manimohan <manimohan.t22@gmail.com>
2024-12-24 17:50:33 +0530

    Installation script updated

--- 12-installation.sh
+++ 12-installation.sh
@@ -5,8 +5,26 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script as a root user"
+    exit 2
 else
     echo "Running this script as a root user"
 fi
 
 dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of MySQL.....FAILED"
+    exit 1
+else
+    echo "Installation of MySQL.....SUCCESS"
+fi
+
+dnf install git -y
+if [ $? -ne 0 ]
+then
+    echo "Installation of GIT.....FAILED"
+    exit 1
+else
+    echo "Installation of GIT.....SUCCESS"
+fi
