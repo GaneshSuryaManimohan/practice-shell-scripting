58c5ec49b3824c3c380b66f7ad7fd63caff6715c
manimohan <manimohan.t22@gmail.com>
2024-12-24 16:28:22 +0530

    made changes to data-types.sh

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "please enter number1:: "
-read NUMBER1
-echo "please enter number2:: "
-read NUMBER2
+echo "please enter 1st number:: "
+read NUM1
+echo "please enter 2nd number2:: "
+read NUM2
 
 
 # NUM1=$1
 # NUM2=$2
 
-SUM=$(($NUMBER1+$NUMBER2))
+SUM=$(($NUM1+$NUM2))
 
-echo "Total of $NUMBER1 and $NUMBER2 is:: $SUM"
+echo "Total of $NUM1 and $NUM2 is:: $SUM"
