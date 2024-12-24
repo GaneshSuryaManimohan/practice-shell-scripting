1eb3826bfaa996eb47f233818bdaa8143593e629
manimohan <manimohan.t22@gmail.com>
2024-12-24 16:31:23 +0530

    data-types2.sh created

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-echo "please enter 1st number:: "
-read NUM1
-echo "please enter 2nd number2:: "
-read NUM2
-
-
-# NUM1=$1
-# NUM2=$2
+NUM1=$1
+NUM2=$2
 
 SUM=$(($NUM1+$NUM2))
 
---
+++ 09-data-types2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "please enter 1st number:: "
+read NUM1
+echo "please enter 2nd number:: "
+read NUM2
+
+
+# NUM1=$1
+# NUM2=$2
+
+SUM=$(($NUM1+$NUM2))
+
+echo "Total of $NUM1 and $NUM2 is:: $SUM"
