8dac1e26c41e10f13f7c2e17bc4028af1f372d9f
manimohan <manimohan.t22@gmail.com>
2024-12-24 17:58:51 +0530

    Installation script updated

--- 12-installation.sh
+++ 12-installation.sh
@@ -28,3 +28,8 @@ then
 else
     echo "Installation of GIT.....SUCCESS"
 fi
+
+#Disadvantages:: 
+#1. In shell scripting the script will not stop even if an error occurs, it is user responsibility to check the previous command is success or not, hence we used the exit status to stop the script from running if there is an error. Only if the exit status is 0 the script will run else it will stop.
+
+#2. In this type code is for every dnf installation, we're creating duplication of if else statements. This can be avoided using functions.
