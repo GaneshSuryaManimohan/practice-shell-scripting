7a93a939d63bd3c647182cd29e4a93ae1c0ba330
manimohan <manimohan.t22@gmail.com>
2024-12-24 16:43:14 +0530

    special-vars.sh created

---
+++ 10-special-vars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All Variables:: $@" #variables should be passed externally
+echo "Number of variables passed:: $#"
+echo "Script Name:: $0"
+echo "Current working directory:: $PWD"
+echo "home directory of current user:: $HOME"
+echo "which user is running the script: $USER"
+echo "Hostname:: $HOSTNAME"
+echo "Process ID of the current shell script: $$"
+echo "Process ID of last background command:: $!"
