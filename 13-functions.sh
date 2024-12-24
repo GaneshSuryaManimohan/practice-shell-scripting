14d46ff024fb2f33ebfa81e52d5a412b80668f2b
manimohan <manimohan.t22@gmail.com>
2024-12-24 19:15:49 +0530

    Functions Script Created

--- 13-functions.sh
+++ 13-functions.sh
@@ -9,10 +9,10 @@ LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2.....FAILURE"
+        echo "$2.....\e[31m FAILURE"
         exit 1
     else
-        echo "$2.....SUCCESS"
+        echo "$2.....\e[32m SUCCESS"
     fi
 }
 
@@ -29,3 +29,10 @@ VALIDATE $? "Installing MYSQL"
 
 dnf install git -y &>>$LOG_FILE
 VALIDATE $? "Installing GIT"
+
+
+## Color Codes ##
+# # \e[31m is for red color
+# \e[32m is for green color
+# \e33m is for yellow color
+# \e[34m is for blue color
