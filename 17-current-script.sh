0b355471116307a134dbbaa5d761e81297632b9e
manimohan <manimohan.t22@gmail.com>
2024-12-27 14:29:11 +0530

    calling other script from current script

--- 17-current-script.sh
+++ 17-current-script.sh
@@ -6,6 +6,6 @@ COURSE="DevOps with AWS"
 echo "Before calling the other script, course:: $COURSE"
 echo "Process ID of the current shell script: $$"
 
-./18-other-script.sh
+sh /18-other-script.sh
 
 echo "After calling other script, course:: $COURSE"
