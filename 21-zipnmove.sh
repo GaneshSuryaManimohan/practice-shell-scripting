aadc5212d11985e65a6646c72ac9e300c9a62a52
manimohan <manimohan.t22@gmail.com>
2024-12-27 18:23:30 +0530

    zipnmove script created

--- 21-zipnmove.sh
+++ 21-zipnmove.sh
@@ -20,7 +20,7 @@ FILES=$(find $SOURCE_DIR -name "*.log" -mtime +2)
 while IFS= read -r line #IFS is Internal Field Separator
 do
     echo "Compressing files: $line"
-    gzip -f $line && mv *.gz /home/ec2-user/test
+    gzip -f $line
 done <<< $FILES
 
 #To find the files older than 14days in current directory:
