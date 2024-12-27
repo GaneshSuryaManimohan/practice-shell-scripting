841cf557bc8562fd845d8bcedca09305ae40ce3d
manimohan <manimohan.t22@gmail.com>
2024-12-27 16:08:04 +0530

    delete old logs script created

--- 20-delete-old-logs.sh
+++ 20-delete-old-logs.sh
@@ -7,9 +7,15 @@ Y="\e[33m"
 
 SOURCE_DIR=/tmp/app-logs
 
-if [ -d $SOURCE_DIR ]
+if [ -d $SOURCE_DIR ] #this will check if the directory exists or not
 then
     echo -e "$G The directory exists $N"
 else
     echo -e "$R $SOURCE_DIR Directory doesn't exist $N"
 fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo "$Y Files older than 14days:: $N"
+
+#To find the files older than 14days in current directory:
+# find . -name "*.log" -mtime +14
