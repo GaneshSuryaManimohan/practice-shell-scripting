84a5691e6cb00c075940e499fc99af650f434441
manimohan <manimohan.t22@gmail.com>
2024-12-27 20:34:11 +0530

    disk usage script created

--- 22-disk-usage.sh
+++ 22-disk-usage.sh
@@ -10,7 +10,9 @@ do
     FOLDER=$(echo $line |awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        MSG+="$FOLDER usage is greater than $DISK_THRESHOLD, current usage is:: $USAGE \n"
+        MSG+="$FOLDER usage is greater than $DISK_THRESHOLD, current usage is:: $USAGE \n" #here MSG+ will append the previous result
     fi
 done <<< $DISK_USAGE
 echo -e "Message: $MSG"
+
+echo "$MESSAGE" | mail -s "Disk Usage Alert" surya.manimohan@gmail.com
