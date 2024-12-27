f3a7df1cdd38199dc4cb921251731cf7148f25e7
manimohan <manimohan.t22@gmail.com>
2024-12-27 18:21:49 +0530

    zipnmove script created

---
+++ 21-zipnmove.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+SOURCE_DIR=/tmp/app-logs
+
+if [ -d $SOURCE_DIR ] #this will check if the directory exists or not
+then
+    echo -e "$G The directory exists $N"
+else
+    echo -e "$R $SOURCE_DIR Directory doesn't exist $N"
+fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +2)
+#echo -e "Files older than 14days in $SOURCE_DIR are:: $Y $FILES $N "
+
+while IFS= read -r line #IFS is Internal Field Separator
+do
+    echo "Compressing files: $line"
+    gzip $line && mv *.gz /home/ec2-user/test
+done <<< $FILES
+
+#To find the files older than 14days in current directory:
+# find . -name "*.log" -mtime +14
