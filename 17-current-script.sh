3e3506de7291758f629c4ef336cd85354e55a915
manimohan <manimohan.t22@gmail.com>
2024-12-27 15:21:00 +0530

    set.sh created

--- 17-current-script.sh
+++ 17-current-script.sh
@@ -8,7 +8,7 @@ echo "Process ID of the current shell script: $$"
 
 #./18-other-script.sh
 
-#we can also use source command to call the other script
+#we can also use source command to call the other script, if we use source command the PID of both current & other script will be same. And variable vaule changes after calling other script
 
 source ./18-other-script.sh
 
@@ -23,4 +23,9 @@ echo "After calling other script, course:: $COURSE"
 # After calling other script, course:: DevOps with AWS
 
 #Output when source ./18-other-script.sh is called from current script
-
+# sh 17-current-script.sh
+# Before calling the other script, course:: DevOps with AWS
+# Process ID of the current shell script: 1509
+# From other script, course:: DevOps+AWS from other script
+# PID of other script:: 1509
+# After calling other script, course:: DevOps+AWS from other script
---
+++ 19-set.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script as a root user"
+    exit 2
+else
+    echo "Running this script as a root user"
+fi
+
+dnf install mysqllala -y
+dnf install git -y
+
+echo "is script still proceeding even after error?"
