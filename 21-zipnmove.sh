5d1aa48451a27b545fe35df96c8db09622f1dde1
manimohan <manimohan.t22@gmail.com>
2024-12-27 19:04:03 +0530

    zipnmove script created

--- 21-zipnmove.sh
+++ 21-zipnmove.sh
@@ -26,6 +26,11 @@ done <<< $FILES
 COMPRSD_FILES=$(find $SOURCE_DIR -name "*.gz")
 echo -e "Moving the compressed files $G $COMPRSD_FILES $N to /home/ec2-user/test"
 mv /tmp/app-logs/*.gz /home/ec2-user/test
+if [ -z "$COMPRS_FILES" ]; 
+then
+    echo -e "$Y No files to compress in $COMPRS_FILES $N"
+    exit 0 # Exit if no files were found
+fi
 
 #To find the files older than 2days in current directory:
 # find . -name "*.log" -mtime +2
